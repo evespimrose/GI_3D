2c2a3a93eb7bad01a68c8c436ee55c585f35c84a
evespimrose <bigjht34@gmail.com>
2024-11-19 09:44:41 +0900

    기본 프로젝트 업로드

---
+++ Mine/Assets/8 LayerMask/Scripts/NewSkillScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Myproject
+{
+    public class NewSkillScript : MonoBehaviour
+    {
+        private void Start()
+        {
+        // ��ų ����
+        }
+    }
+}
