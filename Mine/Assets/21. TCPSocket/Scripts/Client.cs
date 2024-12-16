1b299df0be9ab9dcf7353aaebffca65f29f1928d
evespimrose <evespimrose@github.com>
2024-12-16 16:59:44 +0900

    21.TCPSocket
    과제 포함

---
+++ Mine/Assets/21. TCPSocket/Scripts/Client.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System;
+
+namespace Myproject
+{
+    public class Client : MonoBehaviour
+    {
+        [Header("IP Input")]
+        public TMP_InputField ip;
+        public TMP_InputField port;
+        public Button connect;
+
+        [Header("Message Input")]
+        public TMP_InputField message;
+        public Button send;
+
+        [Header("Text Area")]
+        public RectTransform textArea;
+        public TextMeshProUGUI textPrefab;
+
+        private Thread clientThread;
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        private bool isConnected;
+
+        public static Queue<string> log = new Queue<string>();
+
+        Vector2 point = Vector2.zero;
+
+        private void Awake()
+        {
+            connect.onClick.AddListener(ConnectButtonClick);
+            send.onClick.AddListener(() => { SendSubmit(message.text); });
+            message.onEndEdit.AddListener(SendSubmit);
+        }
+
+        private void Update()
+        {
+            if (log.Count > 0)
+            {
+                TextMeshProUGUI logText = Instantiate(textPrefab, textArea);
+                logText.text = log.Dequeue();
+            }
+
+            // 마우스 클릭 입력 감지
+            if (Input.GetMouseButtonDown(0)) // 0은 왼쪽 마우스 버튼
+            {
+                point = Input.mousePosition;
+                SendMousePosition(point);
+            }
+        }
+
+        private void ClientThread()
+        {
+            try
+            {
+                log.Enqueue("스레드를 시작합니다");
+                TcpClient tcpClient = new TcpClient();
+                IPAddress serverAddress = IPAddress.Parse(ip.text);
+                int portNum = int.Parse(port.text);
+
+                IPEndPoint endPoint = new IPEndPoint(serverAddress, portNum);
+                tcpClient.Connect(endPoint);
+
+                log.Enqueue("서버 접속 성공!~");
+
+                reader = new StreamReader(tcpClient.GetStream());
+                writer = new StreamWriter(tcpClient.GetStream());
+                writer.AutoFlush = true;
+
+                while (tcpClient.Connected)
+                {
+                    string receiveMessage = reader.ReadLine();
+                    log.Enqueue(receiveMessage);
+                }
+
+            }
+            catch (ApplicationException e)
+            {
+                log.Enqueue("어플리케이션 예외 발생");
+                log.Enqueue(e.Message);
+            }
+            catch (Exception e)
+            {
+                log.Enqueue("뭔가 문제가 발생함");
+                log.Enqueue(e.Message);
+            }
+            finally
+            {
+                reader?.Close();
+                writer?.Close();
+            }
+        }
+
+        private void ConnectButtonClick()
+        {
+            log.Enqueue("연결 버튼 눌림");
+            if (!isConnected)
+            {
+                clientThread = new Thread(ClientThread);
+                clientThread.IsBackground = true;
+                clientThread.Start();
+                isConnected = true;
+            }
+            else
+            {
+                clientThread.Abort();
+                isConnected = false;
+            }
+        }
+
+        private void SendSubmit(string message)
+        {
+            if (writer != null && !string.IsNullOrEmpty(message))
+            {
+                writer.WriteLine(message);
+                this.message.text = "";
+            }
+        }
+
+        private void SendMousePosition(Vector2 position)
+        {
+            if (writer != null)
+            {
+                // 좌표 정보를 JSON으로 직렬화
+                Vector2Packet positionData = new Vector2Packet(position.x, position.y);
+                string jsonData = JsonUtility.ToJson(positionData);
+
+                // 서버로 전송
+                SendSubmit(jsonData);
+            }
+        }
+    }
+}
---
+++ Mine/Assets/21. TCPSocket/Scripts/Server.cs
@@ -0,0 +1,188 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace Myproject
+{
+    public class Server : MonoBehaviour
+    {
+        public Button connect;
+        public RectTransform textArea;
+        public TextMeshProUGUI textPrefab;
+        private string ipAddress = "127.0.0.1";
+        public int port = 9999;
+
+        private bool isConnected = false;
+
+        private Thread serverMainThread;
+
+        private int clientId = 0;
+
+        private List<ClientHandler> clients = new List<ClientHandler>();
+
+        public static Queue<string> log = new Queue<string>();
+
+        private void Awake()
+        {
+            connect.onClick.AddListener(ConnectButtonClick);
+        }
+        private void Update()
+        {
+            if (log.Count > 0)
+            {
+                TextMeshProUGUI logText = Instantiate(textPrefab, textArea);
+                logText.text = log.Dequeue();
+            }
+        }
+
+        private void ConnectButtonClick()
+        {
+            if (false == isConnected)
+            {
+                serverMainThread = new Thread(ServerThread);
+                serverMainThread.IsBackground = true;
+                serverMainThread.Start();
+                isConnected = true;
+            }
+            else
+            {
+                serverMainThread.Abort();
+                isConnected = false;
+            }
+        }
+
+        private void ServerThread()
+        {
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+                listener.Start();
+                log.Enqueue("서버 시작");
+                while (true)
+                {
+                    TcpClient tcpClient = listener.AcceptTcpClient();
+
+                    ClientHandler handler = new ClientHandler();
+
+                    handler.Connect(clientId++, this, tcpClient);
+                    clients.Add(handler);
+                    log.Enqueue($"{clientId - 1}번 클라이언트가 접속됨.");
+                }
+            }
+            catch
+            {
+                log.Enqueue("뭔가..뭔가 일어나고 있음");
+            }
+            finally
+            {
+                foreach (var client in clients)
+                    client.Disconnect();
+                serverMainThread.Abort();
+                isConnected = false;
+            }
+
+        }
+
+        public void Disconnect(ClientHandler client)
+        {
+            clients.Remove(client);
+        }
+
+        public void BroadcastToClients(string message)
+        {
+            foreach (ClientHandler client in clients)
+            {
+                client.MessageToClient(message);
+            }
+        }
+
+    }
+
+    public class ClientHandler
+    {
+        public int id;
+        public Server server;
+        public TcpClient tcpClient;
+        public Thread clientHandlerThread;
+        public StreamReader reader;
+        public StreamWriter writer;
+
+        public void Connect(int id, Server server, TcpClient tcpClient)
+        {
+            this.id = id;
+            this.server = server;
+            this.tcpClient = tcpClient;
+            reader = new StreamReader(tcpClient.GetStream());
+            writer = new StreamWriter(tcpClient.GetStream());
+            writer.AutoFlush = true;
+            clientHandlerThread = new Thread(Run);
+            clientHandlerThread.IsBackground = true;
+            clientHandlerThread.Start();
+        }
+
+        public void Disconnect()
+        {
+            writer.Close();
+            reader.Close();
+            tcpClient.Close();
+            clientHandlerThread.Abort();
+            server.Disconnect(this);
+        }
+
+        public void MessageToClient(string message)
+        {
+            writer.WriteLine(message);
+        }
+
+        public void Run()
+        {
+            try
+            {
+                while (tcpClient.Connected)
+                {
+                    string receiveMessage = reader.ReadLine();
+
+                    if (string.IsNullOrEmpty(receiveMessage))
+                        continue;
+
+                    try
+                    {
+                        var positionData = JsonUtility.FromJson<Vector2Packet>(receiveMessage);
+                        string logMessage = $"{id}번 클라이언트가 클릭한 좌표: ({positionData.x}, {positionData.y})";
+                        Server.log.Enqueue(logMessage);
+
+                        server.BroadcastToClients(logMessage);
+                    }
+                    catch
+                    {
+                        server.BroadcastToClients($"{id}님의 말 : {receiveMessage}");
+                    }
+                }
+            }
+            finally
+            {
+                Server.log.Enqueue($"{id}번 클라이언트 연결 종료됨.");
+                Disconnect();
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class Vector2Packet
+    {
+        public float x;
+        public float y;
+
+        public Vector2Packet(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
